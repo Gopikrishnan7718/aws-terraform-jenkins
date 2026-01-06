29862cbb76053a97f0653e6fe584e4f895d2e190
Gopi <gopikrishnan.karuppaiah7718@gmail.com>
2026-01-06 23:23:52 +0530

    enable versioning and force destroy S3

--- terraform/main.tf
+++ terraform/main.tf
@@ -217,5 +217,13 @@ resource "aws_autoscaling_group" "asg" {
 ################################
 resource "aws_s3_bucket" "artifacts" {
   bucket = var.artifact_bucket_name
+  force_destroy = true
 }
 
+resource "aws_s3_bucket_versioning" "artifacts_versioning" {
+  bucket = aws_s3_bucket.artifacts.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
